3f9717a5cf73f2696870cd10f5106fe9480f43bd
gonzalo.daquilema <chaloxavi@hotmail.com>
2024-01-23 16:06:41 -0500

    Init project

---
+++ src/main/java/com/unir/movies/MoviesManagerApplication.java
@@ -0,0 +1,13 @@
+package com.unir.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesManagerApplication.class, args);
+	}
+
+}
